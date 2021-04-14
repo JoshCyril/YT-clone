e0935bb4e2e32abbd595ace57496bf754e0dfe1c
JPC8 <joshua.cyril88@gmail.com>
2021-04-14 08:43:30 +0530

    Done ☑

--- modeToggle.js
+++ modeToggle.js
@@ -1,21 +1,153 @@
 var modeToggle = document.getElementById("modeToggle");
 var logo = document.getElementById("logo");
 var card = document.getElementById("card");
-var search = document.getElementById("search");
+var search = document.getElementById("channel-input");
+var menu = document.getElementById("menu");
+var leftbar = document.getElementById("leftbar");
+var rigthbar = document.getElementById("rigthbar");
+
+var subLine = document.getElementById("subLine");
+
+var navH = document.getElementById("navH");
+var navHico = document.getElementById("navHico");
+var navT = document.getElementById("navT");
+var navTico = document.getElementById("navTico");
+var navS = document.getElementById("navS");
+var navSico = document.getElementById("navSico");
+var navLico = document.getElementById("navLico");
+
+var menuItems = document.getElementsByClassName("menuItems");
+var categoryList = document.getElementsByClassName("categoryList");
+var ai = document.getElementsByClassName("ai");
 
 modeToggle.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
     modeToggle.innerHTML = "brightness_5";
     logo.src = "./assets/yt_logo_rgb_dark.png";
-    card.classList.add("bg-dark");
     search.classList.add("bg-dark");
     search.classList.add("text-white");
   } else {
     modeToggle.innerHTML = "dark_mode";
     logo.src = "./assets/yt_logo_rgb_light.png";
-    card.classList.remove("bg-dark");
     search.classList.remove("bg-dark");
     search.classList.remove("text-white");
   }
+};
+let menuButton = 0;
+
+menu.onclick = function () {
+  menuButton += 1;
+
+  if ((menuOn = menuButton % 2 == 0 ? false : true)) {
+    leftbar.classList.remove("col-lg-2");
+    leftbar.classList.add("col-lg-1");
+
+    rigthbar.classList.remove("col-10");
+    rigthbar.classList.add("col-11");
+
+    [...menuItems].forEach((i) => {
+      i.classList.remove("col-8");
+      i.classList.remove("pl-4");
+      i.classList.add("col-12");
+      i.classList.add("text-center");
+    });
+    [...categoryList].forEach((j) => {
+      j.classList.add("justify-content-center");
+      j.classList.remove("pl-4");
+    });
+  } else {
+    leftbar.classList.add("col-lg-2");
+    leftbar.classList.remove("col-lg-1");
+
+    rigthbar.classList.remove("col-11");
+    rigthbar.classList.add("col-10");
+
+    [...menuItems].forEach((i) => {
+      i.classList.remove("text-center");
+      i.classList.add("col-8");
+      i.classList.add("pl-4");
+      i.classList.remove("col-12");
+    });
+    [...categoryList].forEach((j) => {
+      j.classList.remove("justify-content-center");
+      j.classList.add("pl-4");
+    });
+  }
+};
+
+navH.onclick = function () {
+  [...categoryList].forEach((k) => {
+    if (k.classList.contains("active")) {
+      k.classList.remove("active");
+    } else {
+      navH.classList.add("active");
+      subLine.innerHTML = "Home";
+    }
+  });
+  [...ai].forEach((l) => {
+    if (l.classList.contains("activeIcon")) {
+      l.classList.remove("activeIcon");
+    } else {
+      navHico.classList.add("activeIcon");
+    }
+  });
+};
+
+navT.onclick = function () {
+  [...categoryList].forEach((m) => {
+    if (m.classList.contains("active")) {
+      m.classList.remove("active");
+    } else {
+      navT.classList.add("active");
+      subLine.innerHTML = "Trending";
+    }
+  });
+  [...ai].forEach((n) => {
+    if (n.classList.contains("activeIcon")) {
+      n.classList.remove("activeIcon");
+    } else {
+      navTico.classList.add("activeIcon");
+    }
+  });
+};
+
+navS.onclick = function () {
+  [...categoryList].forEach((o) => {
+    if (o.classList.contains("active")) {
+      o.classList.remove("active");
+    } else {
+      navS.classList.add("active");
+      subLine.innerHTML = "Subscriptions";
+    }
+  });
+  [...ai].forEach((p) => {
+    if (p.classList.contains("activeIcon")) {
+      p.classList.remove("activeIcon");
+    } else {
+      navSico.classList.add("activeIcon");
+    }
+  });
+};
+
+navL.onclick = function () {
+  [...categoryList].forEach((q) => {
+    if (q.classList.contains("active")) {
+      q.classList.remove("active");
+    } else {
+      navL.classList.add("active");
+      subLine.innerHTML = "Library";
+    }
+  });
+  [...ai].forEach((r) => {
+    if (r.classList.contains("activeIcon")) {
+      r.classList.remove("activeIcon");
+    } else {
+      navLico.classList.add("activeIcon");
+    }
+  });
+};
+
+logo.onclick = function () {
+  location.reload();
 };
--- script.js
+++ script.js
@@ -1,13 +1,188 @@
-var modeToggle = document.getElementById("modeToggle");
-var logo = document.getElementById("logo");
+// Options
+const CLIENT_ID =
+  "521764728223-aack9vsuu5j4snbd4lvpsl38njbj5k7s.apps.googleusercontent.com";
+// const CLIENT_ID = null;
+const DISCOVERY_DOCS = [
+  "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest",
+];
+const SCOPES = "https://www.googleapis.com/auth/youtube.readonly";
 
-modeToggle.onclick = function () {
-  document.body.classList.toggle("dark-theme");
-  if (document.body.classList.contains("dark-theme")) {
-    modeToggle.innerHTML = "brightness_5";
-    logo.src = "./assets/yt_logo_rgb_dark.png";
+const authorizeButton = document.getElementById("authorize-button");
+const signoutButton = document.getElementById("signout-button");
+const content = document.getElementById("content");
+const channelForm = document.getElementById("channel-form");
+const channelInput = document.getElementById("channel-input");
+const videoContainer = document.getElementById("video-container");
+
+const defaultChannel = "Apple";
+
+// Form submit and change channel
+channelForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const channel = channelInput.value;
+
+  getChannel(channel);
+});
+
+// Load auth2 library
+function handleClientLoad() {
+  gapi.load("client:auth2", initClient);
+}
+
+// Init API client library and set up sign in listeners
+function initClient() {
+  gapi.client
+    .init({
+      discoveryDocs: DISCOVERY_DOCS,
+      clientId: CLIENT_ID,
+      scope: SCOPES,
+    })
+    .then(() => {
+      // Listen for sign in state changes
+      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+      // Handle initial sign in state
+      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      authorizeButton.onclick = handleAuthClick;
+      signoutButton.onclick = handleSignoutClick;
+    });
+}
+
+// Update UI sign in state changes
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    authorizeButton.style.display = "none";
+    signoutButton.style.display = "block";
+    content.style.display = "block";
+    videoContainer.style.display = "flex";
+    getChannel(defaultChannel);
   } else {
-    modeToggle.innerHTML = "dark_mode";
-    logo.src = "./assets/yt_logo_rgb_light.png";
+    authorizeButton.style.display = "block";
+    signoutButton.style.display = "none";
+    content.style.display = "none";
+    videoContainer.style.display = "none";
   }
-};
+}
+
+// Handle login
+function handleAuthClick() {
+  gapi.auth2.getAuthInstance().signIn();
+  var profile = gapi.auth2.getBasicProfile();
+  // console.log("ID: " + profile.getId());
+  // console.log("Full Name: " + profile.getName());
+  // console.log("Given Name: " + profile.getGivenName());
+  // console.log("Family Name: " + profile.getFamilyName());
+  // console.log("Image URL: " + profile.getImageUrl());
+  // console.log("Email: " + profile.getEmail());
+}
+
+// Handle logout
+function handleSignoutClick() {
+  gapi.auth2.getAuthInstance().signOut();
+}
+// Display channel data
+function showChannelData(data) {
+  const channelData = document.getElementById("channel-data");
+  channelData.innerHTML = data;
+}
+
+// Get channel from API
+function getChannel(channel) {
+  gapi.client.youtube.channels
+    .list({
+      part: "snippet,contentDetails,statistics",
+      forUsername: channel,
+    })
+    .then((response) => {
+      const channel = response.result.items[0];
+
+      const output = `
+      <ul class="collection tect-d" type='none'>
+        <li class="collection-item text-d mb-2"><b>Title:</b> ${
+          channel.snippet.title
+        }</li>
+        <li class="collection-item text-d mb-2"><b>Subscribers:</b> ${numberWithCommas(
+          channel.statistics.subscriberCount
+        )}</li>
+        <li class="collection-item text-d mb-2"><b>Views:</b> ${numberWithCommas(
+          channel.statistics.viewCount
+        )}</li>
+        <li class="collection-item text-d mb-2"><b>Videos:</b> ${numberWithCommas(
+          channel.statistics.videoCount
+        )}</li>
+      </ul>
+      <p class='text-justify text-d'>${channel.snippet.description}</p>
+      <hr>
+      <p class="btn btn-outline-info text-d"><b>Country: </b>${
+        channel.snippet.country
+      }</p>
+    `;
+      tagLine.innerHTML = `Latest Video 💠 ${channel.snippet.title}`;
+      showChannelData(output);
+
+      const playlistId = channel.contentDetails.relatedPlaylists.uploads;
+      requestVideoPlaylist(playlistId);
+    })
+    .catch((err) => alert("No Channel By That Name"));
+}
+
+// Add commas to number
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function requestVideoPlaylist(playlistId) {
+  const requestOptions = {
+    playlistId: playlistId,
+    part: "snippet,contentDetails",
+    maxResults: 10,
+  };
+
+  const request = gapi.client.youtube.playlistItems.list(requestOptions);
+  const tagLine = document.getElementById("tagLine");
+
+  request.execute((response) => {
+    const playListItems = response.result.items;
+    if (playListItems) {
+      let output = "";
+
+      // Loop through videos and append output
+      playListItems.forEach((item) => {
+        const videoId = item.snippet.resourceId.videoId;
+        const Title = item.snippet.title;
+        const ChannelName = item.snippet.channelTitle;
+        const DateNTime = item.snippet.publishedAt.split("T");
+        const Dated = DateNTime[0];
+        const Video = item.snippet.resourceId.kind.split("#");
+        const VideoType = Video[1];
+
+        output += `
+        <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-4">
+             <div class="card" id='card'>
+                <iframe
+                  class="embed-responsive-item img-fluid"
+                  src="//www.youtube.com/embed/${videoId}"
+                  frameborder="0"
+                  allowfullscreen
+                ></iframe>
+                <div class="card-body row">
+
+                  <div class="col-12 p-0">
+                    <h5 class="card-title mb-2">${Title}</h5>
+                    <p class="card-text mb-0">${ChannelName}</p>
+                    <p class="card-text">${Dated} • Type ${VideoType}</p>
+                  </div>
+                </div>
+              </div>
+        </div>
+      `;
+      });
+
+      // Output videos
+      videoContainer.innerHTML = output;
+    } else {
+      videoContainer.innerHTML = "No Uploaded Videos";
+      tagLine.innerHTML = "No Uploaded Videos";
+    }
+  });
+}
